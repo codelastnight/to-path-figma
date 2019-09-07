30054c96e60a7d97b989a599588e21eb66bc8b8c
egirl <fujizhang2525@gmail.com>
2019-09-07 13:11:33 -0700

    BRUHHH

--- code.js
+++ code.js
@@ -7,8 +7,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-//turn svg code junk in to 2 dimentional array of 2, 3, 4 points
+//turn whatever the fuck svg code is into array of points grouped into 4 or 2 ( this is dependant on what type of bezier curve it is. look it up)
+// figma doesnt have the 3 point bezier curve in vector mode, only 4 or 2. 
 var svg2Arr = function (svgData) {
+    /*
+    svgData: the fucking shitty svg path data fuck
+    so like eg [[point1,2,3,4],[4,5],[5,6,7,8]....]
+    i fucking hate this shit
+    */
+    let test = svgData.split(/M|L|C/);
+    console.log(test);
+    let cleanType = [];
+    for (var d in test) {
+        console.log(test[d]);
+        cleanType.push(test[d].trim());
+    }
+    console.log(cleanType);
+    let a = true;
+    while (a) {
+        a = false;
+    }
     return;
 };
 //distance between points a and b
@@ -17,7 +35,7 @@ var distBtwn = function (a, b) {
   a: [x1,y1]
   b: [x2,y2]
   */
-    return Math.sqrt(Math.pow((b[0] - a[0]), 2) + Math.pow((b[1] - a[1]), 2));
+    return Math.sqrt((Math.pow((b[0] - a[0]), 2)) + (Math.pow((b[1] - a[1]), 2)));
 };
 //find point between two points a and b over time
 // in this case time is pixels
@@ -31,8 +49,9 @@ var pointBtwn = function (a, b, t) {
     //find distance between
     const dist = distBtwn(a, b);
     //find the unit vector between points a and b
-    const unitVector = [(a[0] - b[0]) / dist, (a[1] - b[1]) / dist];
-    return [unitVector[0] * t, unitVector[1] * t];
+    // not really unit vector in the math sense tho 
+    const unitVector = [(b[0] - a[0]) / dist, (b[1] - a[1]) / dist];
+    return [a[0] + unitVector[0] * t, a[1] + unitVector[1] * t];
 };
 //calculate De Casteljau’s algorithm from 2-4 points
 // basically turns 4 points on a beizer into a curve
@@ -47,20 +66,24 @@ function pointOnCurve(curve) {
             const dist = distBtwn(curve[c], curve[c + 1]);
             let point = pointBtwn(curve[c], curve[c + 1], (t * dist) / time);
             if (rotation) {
-                const angle = Math.cos(distBtwn(curve[c], curve[c + 1]) / t);
+                let angle = Math.acos(t / dist);
+                //figma wants this number to be in degrees becasue fuck you i guess
+                angle = angle * (180 / Math.PI);
                 point.push(angle);
             }
             arr.push(point);
         }
         return arr;
     };
-    const time = 100;
+    let finalarr = [];
+    const time = 15;
     for (var t = 0; t < time; t++) {
         let arr1 = casteljau(curve, t, time);
         let arr2 = casteljau(arr1, t, time);
         let arr3 = casteljau(arr2, t, time, true);
+        finalarr.push(arr3);
     }
-    return;
+    return finalarr;
 }
 //convert text into indivisual characters
 function text2Curve(node) {
@@ -109,8 +132,26 @@ function main() {
         }
         for (const node of figma.currentPage.selection) {
             if (node.type == 'VECTOR') {
-                console.log(node.vectorNetwork);
-                console.log(node.vectorPaths);
+                console.log(node.vectorPaths[0]);
+                svg2Arr(node.vectorPaths[0].data);
+                //testdatas
+                const testdata = [[1.388586401939392, 21.729154586791992], [-4.074989438056946, 2.2291507720947266], [6.92498779296875, -3.775749444961548], [28.388591766357422, 2.2291524410247803]];
+                var a = pointOnCurve(testdata);
+                console.log(a);
+                console.log(a.length);
+                const newNodes = [];
+                // for (var b =0;b < a.length;b++) {
+                // 	if (isNaN(a[b][0][0])) {
+                // 	} else {
+                // 	const test = figma.createRectangle();
+                // 	test.resize(1,1);
+                // 	test.y=a[b][0][0]
+                // 	test.x=a[b][0][1]
+                // 	test.rotation=a[b][0][2]
+                // 	figma.currentPage.appendChild(test)
+                // 	newNodes.push(test)
+                // 	}
+                // }
             }
             if (node.type == 'TEXT') {
                 //the font loading part
@@ -121,6 +162,7 @@ function main() {
                 text2Curve(node);
             }
         }
+        figma.closePlugin();
     });
 }
 // This shows the HTML page in "ui.html".
@@ -134,5 +176,4 @@ figma.ui.onmessage = msg => {
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
 };
