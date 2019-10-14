e35ae5d07666193bf5091111e97ba740ec1c0b26
not simon <fujizhang2525@gmail.com>
2019-10-13 23:25:46 -0700

    nice

--- src/ts/text.ts
+++ src/ts/text.ts
@@ -30,13 +30,15 @@ export function text2Curve(node: TextNode, pointArr: Array<Point>, curve) {
 		letter.fontName = node.getRangeFontName(i, i + 1)
 		letter.fontSize = node.getRangeFontSize(i, i + 1)
 		letter.characters = safeSpace(charArr[i] + ' ')
+
 		letter.letterSpacing = node.getRangeLetterSpacing(i, i + 1)
+
 		// center the letters
 		//letter.textAlignHorizontal = 'CENTER'
 		letter.textAlignVertical = 'CENTER'
 		letter.textAutoResize = 'WIDTH_AND_HEIGHT'
-
-		console.log(spacing)
+		console.log('letter', letter.characters)
+		console.log('spacing', spacing)
 		let estPoint: Point
 		for (pointIndex; pointIndex < pointArr.length; pointIndex++) {
 			// find nearest point to the length of the word
@@ -64,11 +66,11 @@ export function text2Curve(node: TextNode, pointArr: Array<Point>, curve) {
 		//letter.x = estPoint.x + curve.x
 		//letter.y = estPoint.y + curve.y
 		const centerX = letter.width / 2
-		const centerY = letter.height / 2
+		const centerY = letter.height * 0.8 // change this to change height
 		//spaceing them
 		let angle = ((rotation - 180) * Math.PI) / 180
 		letter.x = 0
-		letter.y = 0
+		letter.y = 0 - centerY
 		// letter.relativeTransform = multiply(
 		// 	move(-letter.width / 2, -0.5 * letter.height),
 		// 	letter.relativeTransform
@@ -77,6 +79,7 @@ export function text2Curve(node: TextNode, pointArr: Array<Point>, curve) {
 		// more code taken from jyc, the god himself https://github.com/jyc http://jyc.eqv.io
 		// Rotate the letter.
 		letter.rotation = 0
+
 		letter.relativeTransform = multiply(rotate(angle), letter.relativeTransform)
 		letter.relativeTransform = multiply(
 			move(estPoint.x + curve.x, estPoint.y + curve.y),
